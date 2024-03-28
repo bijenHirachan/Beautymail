cccbeff17c3cecda530e38db573956844c72edf7
bijenHirachan <76161155+bijenHirachan@users.noreply.github.com>
2024-03-28 11:00:55 +0100

    Implemented the new sendNow method
    https://laravel.com/docs/11.x/upgrade#the-mailer-contract

--- src/Snowfire/Beautymail/Beautymail.php
+++ src/Snowfire/Beautymail/Beautymail.php
@@ -83,6 +83,22 @@ class Beautymail implements Mailer
     }
 
     /**
+     * Send a new message synchronously using a view.
+     *
+     * @param array|Mailable|string $mailable
+     * @param array                 $data
+     * @param Closure|null|string   $callback
+     *
+     * @return SentMessage|null
+     */
+    public function sendNow($view, array $data = [], $callback = null)
+    {
+        $data = array_merge($this->settings, $data);
+
+        return $this->mailer->sendNow($view, $data, $callback);
+    }
+
+    /**
      * Send a new message using the a view via queue.
      *
      * @param string|array    $view
